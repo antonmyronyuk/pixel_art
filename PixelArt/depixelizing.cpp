87e4d9d798fcd341d6e424dc7746f7f29e4e4de8
Anton Myronyuk <antonmyronyuk@gmail.com>
2017-03-20 20:31:48 +0200

    separated visualization functions in other source file

--- PixelArt/depixelizing.cpp
+++ PixelArt/depixelizing.cpp
@@ -1,17 +1,5 @@
 #include "depixelizing.h"
-#include <vector>
-#include <list>
-#include <math.h>
-#include <time.h>
 
-#define NEIGHBOUR_TOP 1
-#define NEIGHBOUR_TOP_RIGHT 2
-#define NEIGHBOUR_RIGHT 4
-#define NEIGHBOUR_BOTTOM_RIGHT 8
-#define NEIGHBOUR_BOTTOM 16
-#define NEIGHBOUR_BOTTOM_LEFT 32
-#define NEIGHBOUR_LEFT 64
-#define NEIGHBOUR_TOP_LEFT 128
 
 Depixelizing::Depixelizing() :
 	m_scale(20) {
@@ -112,67 +100,6 @@ void Depixelizing::buildSimilarityGraph() {
 
 		}
 	}
-
-}
-
-void Depixelizing::showSimilarityGraph() {
-	int scale = int(m_scale);
-	cv::Mat graph(scale * m_height, scale * m_width, CV_8UC3, cvScalar(0,0,0));
-
-	for (int i = 0; i < m_height; ++i) {
-		for (int j = 0; j < m_width; ++j) {
-			if (m_connections[i * m_width + j] & NEIGHBOUR_BOTTOM) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + (i + 1) * scale), cvScalar(255,255,255));
-
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_BOTTOM_RIGHT) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + (j + 1) * scale, (scale + 1) / 2 + (i + 1) * scale), cvScalar(255,255,255));
-				
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_BOTTOM_LEFT) {	
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + (j - 1) * scale, (scale + 1) / 2 + (i + 1) * scale), cvScalar(255,255,255));
-				
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_RIGHT) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + (j + 1) * scale, (scale + 1) / 2 + i * scale), cvScalar(255,255,255));
-				
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_LEFT) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + (j - 1) * scale, (scale + 1) / 2 + i * scale), cvScalar(255,255,255));
-				
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_TOP_RIGHT) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + (j + 1) * scale, (scale + 1) / 2 + (i - 1) * scale), cvScalar(255,255,255));
-				
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_TOP) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + j * scale, (scale + 1) / 2 + (i - 1) * scale), cvScalar(255,255,255));
-			
-			}
-
-			if (m_connections[i * m_width + j] & NEIGHBOUR_TOP_LEFT) {
-				cv::line(graph, cvPoint( (scale + 1) / 2 + j * scale, (scale + 1) / 2 + i * scale), cvPoint((scale + 1) / 2 + (j - 1) * scale, (scale + 1) / 2 + (i - 1) * scale), cvScalar(255,255,255));
-				
-			}
-
-		}
-	}
-
-	for (int i = 0; i < m_height; ++i) {
-		for (int j = 0; j < m_width; ++j) {
-				cv::circle(graph, cv::Point(cvRound((scale + 1) / 2 + j * scale), cvRound((scale + 1) / 2 + i * scale)), 1, cv::Scalar(0, 255, 255), 2);
-		}
-	}
-
-	cv::imshow("SimilarityGraph", graph);
-	cv::waitKey(0);
-
 
 }
 
--- PixelArt/depixelizing.h
+++ PixelArt/depixelizing.h
@@ -2,15 +2,32 @@
 #define DEPIXELIZING_H
 
 #include <opencv2\opencv.hpp>
+#include <vector>
+#include <list>
+#include <math.h>
+#include <time.h>
+
+#define NEIGHBOUR_TOP 1
+#define NEIGHBOUR_TOP_RIGHT 2
+#define NEIGHBOUR_RIGHT 4
+#define NEIGHBOUR_BOTTOM_RIGHT 8
+#define NEIGHBOUR_BOTTOM 16
+#define NEIGHBOUR_BOTTOM_LEFT 32
+#define NEIGHBOUR_LEFT 64
+#define NEIGHBOUR_TOP_LEFT 128
 
 
 class Depixelizing {
+
 public:
+
 	Depixelizing();
 	~Depixelizing();
 
 	void depixelize(const cv::Mat& , float); // main function of the class
+
 private:
+
 	int m_width; // image width
 	int m_height; // image height
 	cv::Mat m_image; // input image
@@ -18,9 +35,12 @@ private:
 	std::vector<unsigned char> m_connections; // similarity graph
 											  // (i, j) -> (i * m_w + j)
 
+	//building
 	void buildSimilarityGraph();
-	void showSimilarityGraph();
 	void deleteFullyConnectedBlocks();
+
+	//visualization
+	void showSimilarityGraph();
 };
 
 #endif //DEPIXELIZING_H
